0a1d0034555bf11786683f8668c14cf004e2f03b
I384733 <I384733@fhict.nl>
2021-04-16 18:47:40 +0200

    Add project files.

---
+++ Rabbit/RabbitConsumer.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShowsService.Rabbit
+{
+    public class RabbitConsumer : BackgroundService
+    {
+        private readonly ILogger<RabbitConsumer> _logger;
+
+        public RabbitConsumer(ILogger<RabbitConsumer> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogDebug($"GracePeriodManagerService is starting.");
+
+            stoppingToken.Register(() =>
+                _logger.LogDebug($" GracePeriod background task is stopping."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogDebug($"GracePeriod task doing background work.");
+
+                // This eShopOnContainers method is querying a database table
+                // and publishing events into the Event Bus (RabbitMQ / ServiceBus)
+
+                await Task.Run(() => UpdateInfo(), stoppingToken);
+            }
+
+            _logger.LogDebug($"GracePeriod background task is stopping.");
+        }
+
+        public void UpdateInfo()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");         //EXCHANGE creation
+
+                var queueName = channel.QueueDeclare().QueueName;                       //QUEUE creation with random name
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: "topic_logs",
+                                  routingKey: "shows.trending");                    //BINDING creation
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>                                     //MESSAGE RECEIVING HANDLER
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey,
+                                      message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TopicReceive.cs
@@ -0,0 +1,54 @@
+﻿/*using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace consumerrabbit
+{
+    public class TopicReceive : BackgroundService
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");         //EXCHANGE creation
+
+                var queueName = channel.QueueDeclare().QueueName;                       //QUEUE creation with random name
+
+                    channel.QueueBind(queue: queueName, 
+                                      exchange: "topic_logs",
+                                      routingKey: "shows.trending");                    //BINDING creation
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>                                     //MESSAGE RECEIVING HANDLER
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey,
+                                      message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+*/
